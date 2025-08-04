f4baaa00b7f8f20f174b9a4a1da78e7f80641e64
basaranmelik <basaranmelik1@gmail.com>
2025-08-04 23:31:47 +0300

    Update global exception

--- src/main/java/com/badsector/anakronik/exception/GlobalExceptionHandler.java
+++ src/main/java/com/badsector/anakronik/exception/GlobalExceptionHandler.java
@@ -7,18 +7,21 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
         String errorMessage = ex.getBindingResult().getFieldErrors().stream()
                 .map(FieldError::getDefaultMessage)
                 .collect(Collectors.joining(", "));
 
-        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+        Map<String, String> errorResponse = Map.of("message", errorMessage);
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(RagValidationException.class)
